9d5e90b705658372b655ede30a9b3caa6bc28052
Vegard Berget <vegardberget@gmail.com>
2023-01-03 11:42:08 +0100

    Documentation+++

--- testrunner/runtests.js
+++ testrunner/runtests.js
@@ -3,16 +3,24 @@ const { LambdaClient } = require("@aws-sdk/client-lambda")
 const TestCollection  = require("./TestCollection")
 const TestRunner = require('./TestRunner')
 const fs = require('fs')
-testRunner("mandelbrot");
-// runTest("arn:aws:lambda:eu-west-1:786903647681:function:PerformanceTestStack-lambdax8664fib512687E7171-jAEjCHTBH7Gr");
 
-async function testRunner(name) {
+runtests()
+
+
+async function runtests() {
+    console.log("Running tests : ")
+    await testRunner("fib",new RegExp(/.*(12586269025).*/));
+    await testRunner("uncompress",new RegExp(/.*(1).*/));
+    await testRunner("mandelbrot",new RegExp(/.*(255).*/));
+}
+
+async function testRunner(name,validatorRegexp) {
     const testCollection = new TestCollection(new CloudFormationClient());
     const testFunctions = await testCollection.getArns(name);
     const testRunner = new TestRunner(new LambdaClient())
     for (const testFunc of testFunctions) {
         console.log(`Running tests for ${testFunc.name}`);
-        testFunc.testResult = await testRunner.runTest(100,testFunc.name,new RegExp(/(.*)/))
+        testFunc.testResult = await testRunner.runTest(100,testFunc.name,validatorRegexp)
     }
     fs.writeFileSync(`../results/${name}.json`,JSON.stringify(testFunctions))
 }
